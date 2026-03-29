aaec65ba6a3528469ec0b5437a799b47337511e0
varshitha v <varshitha.vraj@winds.co.in>
2026-03-29 15:14:31 +0530

    code breaker app

---
+++ CodeBreakerGame/CodeBreakerGame/CodeBreakerGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerGameApp.swift
+//  CodeBreakerGame
+//
+//  Created by Varshitha VRaj on 29/03/26.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreakerGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
